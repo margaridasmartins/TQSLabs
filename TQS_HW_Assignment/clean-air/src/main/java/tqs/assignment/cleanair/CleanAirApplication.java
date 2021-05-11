1be45ab41f45217ae79aca9a61774641b23e6d29
margaridasmartins <margarida.martins@ua.pt>
2021-05-11 15:16:12 +0100

    Homework assignment

---
+++ TQS_HW_Assignment/clean-air/src/main/java/tqs/assignment/cleanair/CleanAirApplication.java
@@ -0,0 +1,13 @@
+package tqs.assignment.cleanair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanAirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanAirApplication.class, args);
+	}
+
+}
---
+++ TQS_HW_Assignment/clean-air/src/test/java/tqs/assignment/cleanair/CleanAirApplicationTests.java
@@ -0,0 +1,13 @@
+package tqs.assignment.cleanair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanAirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
